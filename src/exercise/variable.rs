5bb0d69a1526c4dbebbd0a6a523440ea2958168e
kor0 <ryusama6996@gmail.com>
2023-06-20 21:27:51 +0530

    fix(src): variable exercise

---
+++ src/exercise/variable.rs
@@ -0,0 +1,75 @@
+// variables1.rs
+//
+// Make me compile!
+//
+// Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
+// hint.
+// I AM NOT DONE
+fn main() {
+    let x:u32 = 5;
+    println!("x has the value {}", x);
+}
+// variables2.rs
+//
+// Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
+// hint.
+// I AM NOT DON
+fn main() {
+    let x:u32 = 12;
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
+// variables3.rs
+//
+// Execute `rustlings hint variables3` or use the `hint` watch subcommand for a
+// hint.
+// I AM NOT DONE
+fn main() {
+    let x: i32 = 12;
+    println!("Number {}", x);
+}
+// variables4.rs
+//
+// Execute `rustlings hint variables4` or use the `hint` watch subcommand for a
+// hint.
+// I AM NOT DONE
+fn main() {
+    let mut x:u32 = 3;
+    println!("Number {}", x);
+    x = 5; // don't change this line
+    println!("Number {}", x);
+}
+// variables5.rs
+//
+// Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
+// hint.
+// I AM NOT DON
+fn main() {
+    let number = "T-H-R-E-E"; // don't change this line
+    println!("Spell a Number : {}", number);
+    {
+        let number = 3; // don't rename this variable
+        println!("Number plus two is : {}", number + 2);
+    }
+}
+// variables6.rs
+//
+// Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
+// hint.
+// I AM NOT DONE
+const NUMBER = 3;
+fn main() {
+    println!("Number {}", NUMBER);
+}
+// variables6.rs
+//
+// Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
+// hint.
+// I AM NOT DO
+const NUMBER:i32 = 3;
+fn main() {
+    println!("Number {}", NUMBER);
+}
